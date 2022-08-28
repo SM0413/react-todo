ca8bd3bcdaa29cb85ac3b7b1157e2bf444cc5ccb
SM0413 <kbjtmdals@naver.com>
2022-08-29 07:57:48 +0900

    Form Validation

--- src/ToDoList.tsx
+++ src/ToDoList.tsx
@@ -1,16 +1,47 @@
 import { useForm } from "react-hook-form";
 export function ToDoList() {
-  const { register, watch } = useForm();
-  console.log(watch());
+  const { register, watch, handleSubmit, formState } = useForm();
+  const onValid = (data: any) => {
+    console.log(data);
+  };
+  console.log(formState.errors);
+  // console.log(watch());
   return (
     <div>
-      <form>
-        <input {...register("Email")} placeholder="Email" />
-        <input {...register("firstName")} placeholder="FirstName" />
-        <input {...register("lastName")} placeholder="LastName" />
-        <input {...register("userName")} placeholder="UserName" />
-        <input {...register("password")} placeholder="Password" />
-        <input {...register("password1")} placeholder="Password1" />
+      <form
+        style={{ display: "flex", flexDirection: "column" }}
+        onSubmit={handleSubmit(onValid)}
+      >
+        <input {...register("email", { required: true })} placeholder="Email" />
+        <input
+          {...register("firstName", { required: true })}
+          placeholder="FirstName"
+        />
+        <input
+          {...register("lastName", { required: true })}
+          placeholder="LastName"
+        />
+        <input
+          {...register("userName", { required: true, minLength: 10 })}
+          placeholder="UserName"
+        />
+        <input
+          {...register("password", {
+            required: true,
+            minLength: {
+              value: 5,
+              message: "Your password is to short",
+            },
+          })}
+          placeholder="Password"
+        />
+        <input
+          {...register("password1", {
+            required: "Password is required",
+            minLength: 5,
+          })}
+          placeholder="Password1"
+        />
         <button>Add</button>
       </form>
     </div>
